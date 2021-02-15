9aa3460c0b99a366e21a7a8087dbde99d1c308f8
jatink124 <kaushaljatin48@gmail.com>
2021-02-15 16:02:20 +0530

    first commit

---
+++ php-react/db_connection.php
@@ -0,0 +1,3 @@
+<?php
+//$db_conn = mysqli_connect("localhost","root","","findsarkarijob");
+$db_conn = mysqli_connect("194.59.164.180","u151583556_username_job","sarkaridb","u151583556_sarkarijob");
